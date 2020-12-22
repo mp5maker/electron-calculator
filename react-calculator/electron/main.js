c7ac01b786fcfb2eb7a464aa84090f755673ea6e
Shabuktagin Photon Khan <khan.photon@gmail.com>
2020-12-22 20:56:26 +0600

    feat: now the electron for both development and production react builds

--- react-calculator/electron/main.js
+++ react-calculator/electron/main.js
@@ -3,6 +3,9 @@ const path = require('path')
 const url = require('url')
 
 let mainWindow;
+const isDevelopment = process.env.ELECTRON_START_DEVELOPMENT ? true : false;
+console.log(isDevelopment)
+
 function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
@@ -11,7 +14,13 @@ function createWindow() {
             nodeIntegration: true
         }
     })
-    mainWindow.loadURL('http://localhost:3000')
+    const startURL = isDevelopment ? 'http://localhost:3000'
+        : url.format({
+            pathname: path.join(__dirname, '..', 'build/index.html'),
+            protocol: 'file:',
+            slashes: true
+        })
+    mainWindow.loadURL(startURL)
     mainWindow.webContents.openDevTools()
     mainWindow.on('closed', function() {
         mainWindow = null
