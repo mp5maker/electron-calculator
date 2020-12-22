97f03a798137189906cb9da8cd05b7837373f6a7
Shabuktagin Photon Khan <khan.photon@gmail.com>
2020-12-22 20:36:32 +0600

    feat: now eletorn and react app runs together

---
+++ react-calculator/electron/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+
+let mainWindow;
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    mainWindow.loadURL('http://localhost:3000')
+    mainWindow.webContents.openDevTools()
+    mainWindow.on('closed', function() {
+        mainWindow = null
+    })
+}
+
+
+app.whenReady().then(async() => {
+    await createWindow()
+})
+
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function() {
+    if (mainWindow === null) createWindow()
+})
