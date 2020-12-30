354b4e0da7eb5e7d381b08501bc9f421d330c436
Shabuktagin Photon Khan <khan.photon@gmail.com>
2020-12-31 01:01:45 +0600

    feat: now the calculator calculates properly for the integers

--- react-calculator/src/utilities/numberHelper.tsx
+++ react-calculator/src/utilities/numberHelper.tsx
@@ -24,16 +24,31 @@ export const numberHelper = {
   divide,
   modulo,
   performCalculation: (list: Array<string>) => {
+    console.log(list)
     if (Array.isArray(list) && list.length > 0) {
-      const { result }: any = list.reduce(
+      const { result, value, operator }: any = list.reduce(
         (all: any, item: string) => {
           if (all.operator && all.value) {
             let calculate = 0
-            if (item === '+') calculate = add({ first: all.value, second: parseFloat(item) })
-            if (item === '-') calculate = subtract({ first: all.value, second: parseFloat(item) })
-            if (item === '*') calculate = multiply({ first: all.value, second: parseFloat(item) })
-            if (item === '/') calculate = divide({ first: all.value, second: parseFloat(item) })
-            if (item === '%') calculate = modulo({ first: all.value, second: parseFloat(item) })
+            if (all.operator === '+') {
+              calculate = add({ first: all.value, second: parseFloat(item) })
+            }
+            if (all.operator === '-') {
+              calculate = subtract({ first: all.value, second: parseFloat(item) })
+            }
+            if (all.operator === '*') {
+              calculate = multiply({ first: all.value, second: parseFloat(item) })
+            }
+            if (all.operator === '/') {
+              calculate = divide({ first: all.value, second: parseFloat(item) })
+            }
+            if (all.operator === '%') {
+              calculate = modulo({ first: all.value, second: parseFloat(item) })
+            }
+            console.log('🚀 ~ file: numberHelper.tsx ~ line 30 ~ item', item)
+            console.log('🚀 ~ file: numberHelper.tsx ~ line 42 ~ calculate', calculate)
+            console.log('🚀 ~ file: numberHelper.tsx ~ line 43 ~ all.value', all.value)
+            console.log('🚀 ~ file: numberHelper.tsx ~ line 44 ~ all.operator', all.operator)
 
             return {
               operator: '',
@@ -45,7 +60,7 @@ export const numberHelper = {
           if (isOperator(item)) {
             return {
               ...all,
-              operator: ''
+              operator: item
             }
           }
 
@@ -60,6 +75,25 @@ export const numberHelper = {
           result: 0
         }
       )
+
+      if (value) {
+        if (operator === '+') {
+          return add({ first: result, second: parseFloat(value) })
+        }
+        if (operator === '-') {
+          return subtract({ first: result, second: parseFloat(value) })
+        }
+        if (operator === '*') {
+          return multiply({ first: result, second: parseFloat(value) })
+        }
+        if (operator === '/') {
+          return divide({ first: result, second: parseFloat(value) })
+        }
+        if (operator === '%') {
+          return modulo({ first: result, second: parseFloat(value) })
+        }
+      }
+
       return result
     }
     return false
